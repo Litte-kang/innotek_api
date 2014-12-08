c5fdcf8eda8afc257c32af37e3434cdec8f2286e
David Liu <160386@qq.com>
2014-12-08 09:35:30 +0800

    k

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ server.use(
   function crossOrigin(req,res,next){
   	res.header("Access-Control-Allow-Credentials", true);
     //res.header("Access-Control-Allow-Origin", "http://www.fushuile.com");
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     return next();
   }
