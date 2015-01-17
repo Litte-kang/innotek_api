264d8d3cb06933ae01da331bfbaf1b8178bb39a9
Rui Liu <liurui81@gmail.com>
2015-01-17 11:49:50 +0800

    bu

--- server.js
+++ server.js
@@ -238,7 +238,7 @@ server.post('/commands', function(req, res, next){
 			console.log('New Command is: ' + command);
 			var info = '{"type":12,"address":"0000000001","data":[0,0,{"DryBulbCurve":[11,22,33,44,55,66,77,88,99,0]},{"WetBulbCurve":[32.5,32.5,32.5,32.5,32.5,32.5,32.5,32.5,32.5,32.5]},{"TimeCurve":[32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32]}]}';
 		}
-			SendCmdInfo(8125, command.ip , info);
+			client.SendCmdInfo(8125, command.ip , info);
 			next();
 	})
 });
