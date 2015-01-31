34a00b9545a054df94b3fe99d85de14b98e71d76
David Liu <160386@qq.com>
2015-01-31 12:58:31 +0800

    d

--- server.js
+++ server.js
@@ -265,8 +265,8 @@ server.post('/commands', function(req, res, next){
 
 	var json = MakeConfigCurve(midAddress, address, drys, wets, times);
 	console.log(ip);
-	console.log(json);
-	
+	console.log(JSON.stringify(json));
+
 	client.SendCmdInfo(8125, ip, json);
 
 	res.send(200,"ok");
