435e1965025422299fa472e9db68a158572e6071
Litte-kang <pengkang1212@sina.com>
2015-03-23 17:18:27 +0800

    Update command.js

--- db/schemas/command.js
+++ db/schemas/command.js
@@ -7,8 +7,24 @@ var schema = new Schema({
 	infoType: 		{type: Number, default: 12},
 	address:   		String,
 	midAddress:     String,
-	curves: 	    Schema.Types.Mixed,
+	command: 	    Schema.Types.Mixed,
 	createdAt:  	{type: Date, default: Date.now}
 });
 
-module.exports = mongoose.model('Command', schema);
+var Command = mongoose.model('Command', schema, 'Command');
+
+module.exports.AddCommand = function(options, doc){
+
+	Command.findOneAndUpdate(options, doc, {upsert: true}, function(err, data){
+						   
+   		if(err){
+   			console.log('command saved failed');
+   			res.send(500);
+   			next(err);
+   		}else{
+   			console.log('command saved success');
+   			res.send(200);
+			next()
+   		}
+	});	
+};
